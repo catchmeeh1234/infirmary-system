aab16ea8f07927a3cc2118dd76f69b5baeda4b08
miguel <juanmiguel21salonga@gmail.com>
2023-06-20 08:43:53 +0800

    changes

--- src/app/services/pr.service.ts
+++ src/app/services/pr.service.ts
@@ -102,4 +102,9 @@ export class PrService {
     console.log(prno);
     return this.http.get(API_URL + `/loadPrAndItems.php?prno=${prno}`, {responseType: 'json'});
   }
+
+  //* monthly total pr */
+  PRTotalJan() {
+    return this.http.get(API_URL + '/api/JanTotalPR.php', {responseType: 'json'});
+  }
 }
